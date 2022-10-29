73784273b90617f5e874b3a20d15123a2fe3ca81
AlulaLeak <alula.leakemariam94@gmail.com>
2022-10-28 23:56:44 -0400

    turned hardcoded sidemenu into mapped array

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,87 +1,44 @@
 import Link from "next/link";
 
+interface barType {
+  title: string;
+
+  icon: string;
+
+  link: string;
+}
+
+const foo: barType[] = [
+  { title: "Dashboard", icon: "home", link: "/dashboard" },
+  { title: "Classes", icon: "class", link: "/dashboard" },
+  { title: "Instructors", icon: "assignment_ind", link: "/dashboard" },
+  { title: "Guests", icon: "person_outline", link: "/dashboard" },
+  { title: "Locations", icon: "place", link: "/dashboard" },
+  { title: "Reporting", icon: "visibility", link: "/dashboard" },
+  { title: "Reporting", icon: "visibility", link: "/dashboard" },
+  { title: "Reporting", icon: "visibility", link: "/dashboard" },
+];
+
 export default function SideNav() {
   return (
     <section className="fl p2 w-20-ns w-10-m w-20 bg-ez-light-black pa0 ma0 h-100 white-80">
       <div className="db w-100 bg-ez-dark-black mv-2">
-        <h3 className="f3 ma0 pa2">Brand</h3>
+        <h3 className="f3 ma0 pa2 black">Brand</h3>
       </div>
       <ul className="list pa1 center">
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">home</i>
-          <Link
-            className="link pa2 pointer white dn-m v-mid"
-            href="/dashboard/addnewuser"
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">class</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Classes
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">assignment_ind</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Instructors
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">person_outline</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Guests
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">place</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Locations
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
-        <li className="tc-m hover-bg-blue pa3 pointer">
-          <i className="material-icons v-mid">visibility</i>
-          <Link className="link pa2 pointer white dn-m v-mid" href="#">
-            Reporting
-          </Link>
-        </li>
+        {foo.map((val: barType, idx: number) => {
+          return (
+            <li key={idx} className="tc-m hover-bg-blue pa3 pointer">
+              <i className="material-icons v-mid">{val.icon}</i>
+              <Link
+                className="link pa2 pointer black dn-m v-mid"
+                href={val.link}
+              >
+                {val.title}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
